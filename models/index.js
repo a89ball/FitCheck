a36aff970fcafc58f06330d87a6eab3ab8f15636
mahmoud cali <mahmoudcali2@gmail.com>
2021-09-22 23:34:57 +0100

    deletd all the files and created new ones with content

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user')
-const Workout = require ('./workout')
-
-User.hasMany(Workout,{
-    foreignKey: userId,
-    onDelete: 'CASCADE',
-})
-
-module.exports = {User, Workout}
